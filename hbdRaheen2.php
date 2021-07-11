f70dea1153a9fe6cd29f76edd98edf935bb97fab
nkazi050 <87252466+nkazi050@users.noreply.github.com>
2021-07-11 08:50:37 +0530

    Add files via upload

---
+++ hbdRaheen2.php
@@ -0,0 +1,67 @@
+<html>
+    <head>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Langar&display=swap" rel="stylesheet">
+
+        <style>
+
+            body {
+                margin: 0% auto;
+                background: linear-gradient(0deg, #e37878 20%,#97dfe6 100%);
+                letter-spacing: 1px;
+                line-height: 1.8;
+                font-family: 'Courgette', cursive;
+                align-content: center;
+            }
+
+            .vdo{
+                width: 100%;
+                height: 95%;
+            }
+
+            .txt{
+                border: 2px white solid;
+                width: 100%;
+                height: 5%;
+            }
+
+            #img1 {
+                width: 100%; 
+                height: 100%;
+            }
+
+            .heading {
+                text-align:center; 
+                color:White;
+                margin-top: 2%;
+                padding:0;
+            }
+
+        </style>
+
+    </head>
+
+    <body>
+
+    <?php ?>
+
+        <div class="vdo">
+
+            <video width="1080" height="1920" controls id="img1">
+                <source src="bday.mp4" type="video/mp4">
+            </video> 
+
+        </div>
+
+        <div class="txt">
+
+            <h2 class="heading">Happy Birthday Once Again Raheen <span class="etn">:D</span> !!</h2>
+
+        </div>
+
+    </body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once("hbdRaheen.html");
+
+?>
